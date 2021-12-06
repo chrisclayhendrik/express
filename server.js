f411d11ad8b3c4c29cf41d30bed1b805f91ba511
Chris <chrisclayhendrik@gmail.com>
2021-12-06 11:57:50 -0600

    Update with dynamic route

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+// route to a dynamic path
+
+app.get("/now", (request, response) => {
+    const date = new Date();
+    response.send(date);
+  });
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
